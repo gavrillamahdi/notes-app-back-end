0a77694b0921a9675c448aa94fb0a75ecca3e0c4
Gavrilla Mahdi Ghozali <gavrillamahdi@gmail.com>
2022-05-03 08:59:32 +0700

    Menampilkan Catatan: https://www.dicoding.com/academies/261/tutorials/14783

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,23 @@
-const { addNoteHandler } = require('./handler');
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+} = require('./handler');
 
 module.exports = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
   },
 ];
